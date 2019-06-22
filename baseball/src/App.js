39fab2fd113a3a4b6769bc5e78c08a4f3a1ce522
Mychal <hallmychal@gmail.com>
2019-06-22 13:06:40 -0500

    Dashboard rendered. nice

--- baseball/src/App.js
+++ baseball/src/App.js
@@ -1,10 +1,11 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import Dashboard from "./components/Dashboard.js";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-
+      <Dashboard />
     </div>
   );
 }
